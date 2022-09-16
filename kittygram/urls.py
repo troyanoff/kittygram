0f88892e43455033f67dc75ed7c81e746d66d0c6
Юрий Троянов <troyan0v@yandex.ru>
2022-09-16 19:42:01 +0300

    16.09

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -13,4 +13,6 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ] 
